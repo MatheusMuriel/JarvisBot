10385f0d4508908f7df40f23aae5c57105980a37
Muriel <matheus.muriel@outlook.com>
2019-12-16 16:41:56 -0300

    Resposta do callback

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import logging
 
 from callbacks import *
-#from skills import *
 import skills
 
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
@@ -11,31 +10,41 @@ from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQ
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
+updater = None
+user_id = None
+api_token = None
 
 print('Iniciando Jarvis...')
 
 # Le as credenciais
-c_dir = os.path.dirname(__file__)
-with open(os.path.join(c_dir, "config/credenciais.txt")) as key_file:
-  api_token, user_id = key_file.read().splitlines()
+def carrega_credenciais():
+  dir_atual = os.path.dirname(__file__)
+  caminho_credenciais = os.path.join(dir_atual, "config", "credenciais.txt")
+  
+  # Verifica se o arquivo existe
+  if os.path.isfile(caminho_credenciais):
+    with open(caminho_credenciais) as arquivo_ultra_secreto:
+      credenciais = arquivo_ultra_secreto.read().splitlines()
 
-user_id = int(user_id)
+    global api_token, user_id
+    api_token = credenciais[0]
+    user_id = int(credenciais[1])
+  else:
+    print("Arquivo 'config/credenciais.txt' não foi encontrado")
+    exit()
 
 def start(update, context):
   if update.message.from_user.id == user_id:
-    # Valido
     update.message.reply_text('Olá mestre! :D')
-    #mostrar_opcoes(update)
   else:
-    # Não é valido
     update.message.reply_text('Grrrr!\nVocê não é meu mestre')
 
 def opcoes(update, context):
   button_list = [
-    [InlineKeyboardButton(nomes_bonitos.get(STATUS_SERVIDOR), callback_data=valor_callback(STATUS_SERVIDOR)),
-      InlineKeyboardButton(nomes_bonitos.get(TRABS_ENTREGAR), callback_data=valor_callback(TRABS_ENTREGAR))],
-    [InlineKeyboardButton(nomes_bonitos.get(PREV_TEMPO), callback_data=valor_callback(PREV_TEMPO)),
-      InlineKeyboardButton(nomes_bonitos.get(ESTOQ_RACAO), callback_data=valor_callback(ESTOQ_RACAO))],
+    [InlineKeyboardButton(nomes_bonitos.get(STATUS_SERVIDOR), callback_data=valor_callback.get(STATUS_SERVIDOR)),
+      InlineKeyboardButton(nomes_bonitos.get(TRABS_ENTREGAR), callback_data=valor_callback.get(TRABS_ENTREGAR))],
+    [InlineKeyboardButton(nomes_bonitos.get(PREV_TEMPO), callback_data=valor_callback.get(PREV_TEMPO)),
+      InlineKeyboardButton(nomes_bonitos.get(ESTOQ_RACAO), callback_data=valor_callback.get(ESTOQ_RACAO))],
   ]
 
   reply_markup = InlineKeyboardMarkup(button_list)
@@ -52,16 +61,27 @@ def processa_botao(update, context):
   if opcao_escolhida == TRABS_ENTREGAR:
     resposta = skills.trabalhos_a_entregar()
 
-  query.edit_message_text(text="{}: \n{}".format(nomes_bonitos.get(opcao_escolhida), resposta))
+  #query.edit_message_text(text="{}: \n{}".format(nomes_bonitos.get(opcao_escolhida), resposta))
+  resposta_callback = "{}: \n{}".format(nomes_bonitos.get(opcao_escolhida), resposta)
+  query.message.reply_text(resposta_callback)
+
+  # Linha abaixo é para evitar o bug do relogio ficar aparecendo em cima do botão
+  context.bot.answer_callback_query(query.id, text='')
 
 def error(update, context):
   """Log Errors caused by Updates."""
   logger.warning('Update "%s" caused error "%s"', update, context.error)
 
+def enviar_mensagem(mensagem):
+  global updater
+  updater.bot.send_message(user_id, mensagem)
+
 def main():
   # Cria o Updater e passa para ele o token do bot
   # O "use_context" é marcado como verdadeiro para
   # usar o callback
+  global updater
+
   updater = Updater(api_token, use_context=True)
 
   updater.dispatcher.add_handler(CommandHandler('start', start))
@@ -77,4 +97,5 @@ def main():
   updater.idle()
 
 if __name__ == '__main__':
+  carrega_credenciais()
   main()
